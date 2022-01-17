ce339763e85c7b7f01214bb26254dfff1bd73e03
Victor Turitzin <turitzinv@gmail.com>
2022-01-16 20:14:56 -0500

    commit tree 3d56eaffd28f26d0489791027e2b65ead01dcb1d

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
+  static capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+  static sanitize(word) {
+    return word.replace(/[^a-zA-Z0-9\s'-]/gi, "")
+  }
+  static titleize(word) {
+    let array = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let sentence = word.split(' ').map(s => {
+      if (!array.includes(s)) {
+        return Formatter.capitalize(s)
+      } else {
+        return s
+      }
+    }).join(' ')
+    return Formatter.capitalize(sentence)
+  }
 }
